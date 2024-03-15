6264c77bf70314ba8199b54d2b5306c81bb1e28f
blaztted <bbc.silva@gmail.com>
2024-03-15 16:23:20 +0000

    added project and updated project cards

--- react-portfolio/src/components/ProjectCard.js
+++ react-portfolio/src/components/ProjectCard.js
@@ -3,7 +3,7 @@ import Button from "react-bootstrap/Button";
 import projImg1 from "../assets/img/flavourfinder.png";
 import projImg2 from "../assets/img/kickspot-print.png";
 import projImg3 from "../assets/img/forkify.png";
-import projImg4 from "../assets/img/weather.png";
+import projImg4 from "../assets/img/galactic.png";
 import projImg5 from "../assets/img/readme.png";
 import projImg6 from "../assets/img/react-portfolio.png";
 
@@ -22,10 +22,10 @@ export const ProjectCard = ({
           <h4>{title}</h4>
           <span>{description}</span>
           <div className="deplinks">
-            <Button variant="light" a href={deployLink} target="blank">
+            <Button id="but" variant="light" a href={deployLink} target="blank">
               WebSite
             </Button>{" "}
-            <Button variant="light" a href={githubLink} target="blank">
+            <Button id="but" variant="light" a href={githubLink} target="blank">
               Github
             </Button>{" "}
           </div>
@@ -37,49 +37,45 @@ export const ProjectCard = ({
 
 export const ProjectsList = [
   {
-    title: "Flavour Finder",
+    title: "React Portfolio",
     description: "Design & Development",
-    imgUrl: projImg1,
-    deployLink: "https://blaztted.github.io/flavour-finder/",
-    githubLink: "https://github.com/blaztted/flavour-finder?tab=readme-ov-file",
+    imgUrl: projImg6,
+    deployLink: "https://react-portfolio-bruno.netlify.app/",
+    githubLink: "https://github.com/blaztted/react-portfolio",
   },
   {
-    title: "Kickspot Ecommerce",
+    title: "Galactic Getaways",
     description: "Design & Development",
-    imgUrl: projImg2,
-    deployLink: "https://github.com/blaztted/kickspot-ecommerce",
-    githubLink: "https://github.com/blaztted/kickspot-ecommerce",
+    imgUrl: projImg4,
+    deployLink: "https://galactic-getaways.netlify.app/home",
+    githubLink: "https://github.com/MoskaDowlatzay/galactic-getaways",
   },
   {
     title: "Forkify",
     description: "Design & Development",
     imgUrl: projImg3,
     deployLink: "https://forkify-brunofinal.netlify.app/",
-    githubLink: "https://forkify-brunofinal.netlify.app/",
+    githubLink: "https://github.com/blaztted/forkify",
   },
   {
-    title: "Weather Dashboard",
+    title: "Kickspot Ecommerce",
     description: "Design & Development",
-    imgUrl: projImg4,
-    deployLink: "https://blaztted.github.io/js-weather-dashboard/",
-    githubLink: "https://github.com/blaztted/js-weather-dashboard",
+    imgUrl: projImg2,
+    githubLink: "https://github.com/blaztted/kickspot-ecommerce",
   },
   {
     title: "Github Profile README Generator",
     description: "Development",
     imgUrl: projImg5,
-    deployLink: "https://github.com/blaztted/README-generator",
     githubLink: "https://github.com/blaztted/README-generator",
   },
   {
-    title: "React Portfolio",
+    title: "Flavour Finder",
     description: "Design & Development",
-    imgUrl: projImg6,
-    deployLink: "",
-    githubLink: "https://github.com/blaztted/react-portfolio",
+    imgUrl: projImg1,
+    deployLink: "https://blaztted.github.io/flavour-finder/",
+    githubLink: "https://github.com/blaztted/flavour-finder?tab=readme-ov-file",
   },
 ];
 
-// export const ProjectsList = projectsData;
-// export default ProjectCard;
 export default ProjectsList;
